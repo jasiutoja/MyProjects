ba98078d5af227e2942583a53543e0a769eecfbf
jasiu <jasiu@192.168.1.100>
2018-01-28 13:44:34 +0100

    test of commit

---
+++ PrimeNumbers/src/jasiutoja/primenumbers/generator/impl/BasicPrimeNumbersGenerator.java
@@ -0,0 +1,15 @@
+package jasiutoja.primenumbers.generator.impl;
+
+import java.util.List;
+
+import jasiutoja.primenumbers.generator.PrimeNumbersGenerator;
+
+public class BasicPrimeNumbersGenerator implements PrimeNumbersGenerator {
+
+	@Override
+	public List<String> generatePrimeNumbersInRange(String from, String to) {
+		
+		return null;
+	}
+	
+}
